396309784828bcfd26ab57f570d376ac247dfef0
EmreKaratas64 <emrekaratas076@gmail.com>
2024-04-06 17:08:52 +0200

    Seperating pre-processing of dataset to different file. Start of Feature Selection

---
+++ All_Pre-Processing.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 16:48:26 2024
+
+@author: EMRE KARATAŞ
+"""
+
+import pandas as pd 
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
+from sklearn.impute import SimpleImputer
+
+titanic = pd.read_csv("train.csv")
+categorical_features = ['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked']
+
+#print(titanic.describe())
+#print(titanic.info())
+
+
+# need to save below processings to CSV file ???
+
+# MinMax Scaling
+
+numeric_cols = titanic.select_dtypes(include=[np.number]).columns
+X = titanic[numeric_cols]
+
+norm = MinMaxScaler(feature_range=(0,1)).fit(X)
+X_minmax = pd.DataFrame(norm.transform(X), columns=X.columns)
+
+# Standardization
+
+scale = StandardScaler().fit(X)
+X_scaled = pd.DataFrame(scale.transform(X), columns=X.columns)
+
+print(X_minmax.describe().round(3))
+print("\n\n")
+print(X_scaled.describe().round(3))
+
+
+# Imputing Data
+
+imputer = SimpleImputer(strategy='mean')  # Replace with mean
+X_imputed = imputer.fit_transform(X)
+
+# Create normalizers for L-1 and L-2
+normalizer_l1 = Normalizer(norm='l1')
+normalizer_l2 = Normalizer(norm='l2')
+
+# Normalize data using L-1 and L-2
+normalized_data_l1 = normalizer_l1.fit_transform(X_imputed)
+normalized_data_l2 = normalizer_l2.fit_transform(X_imputed)
+
+
+
+# Observe the normalized data
+print("\n\nOriginal data:\n", X[:5])  # Print the first 5 rows
+
+print("\nL-1 normalized data:\n", normalized_data_l1[:5])
+print("\nL-1 norm sum of each column:\n", normalized_data_l1.sum(axis=0))  # Check sum of absolute values
+
+print("\n\nL-2 normalized data:\n", normalized_data_l2[:5])
+print("\nL-2 norm sum of squares of each column:\n", (normalized_data_l2**2).sum(axis=0))  # Check sum of squares
+
+
+"""
+Why we don't use Discretization'
+Limited Benefit for Most Features: In the Titanic dataset, many features (e.g., Age, Fare) likely already have a clear meaning and can be used directly in models. Discretization might not add much value.
+Potential Information Loss: Discretization can group together data points that might have different behaviors, leading to information loss.
+Choosing Cut Points: Defining meaningful cut points for discretization can be subjective and impact model performance.
+"""
+
+
+def binarize_age(age):
+    if age>=18:
+        return 1 # Adult
+    else:
+        return 0 # Child
+
+titanic['IsAdult'] = titanic['Age'].apply(binarize_age)
+
+def binarize_lonliness(row):
+    if row['SibSp']+row['Parch']==0:
+        return 1 # Alone
+    elif row['SibSp']+row['Parch']>0:
+        return 0 # Not Alone
+
+titanic['IsAlone'] = titanic.apply(binarize_lonliness, axis=1)
+
+titanic.to_csv("processed_titanic.csv", index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Project.py
+++ Project.py
@@ -1,95 +1,54 @@
+from sklearn import datasets, feature_selection
+from itertools import compress
 import pandas as pd 
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
 from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import VarianceThreshold
 
 titanic = pd.read_csv("train.csv")
-categorical_features = ['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked']
-
-#print(titanic.describe())
-#print(titanic.info())
-
-
-# need to save below processings to CSV file ???
-
-# MinMax Scaling
-
 numeric_cols = titanic.select_dtypes(include=[np.number]).columns
-X = titanic[numeric_cols]
+numeric_titanic = titanic[numeric_cols]
 
-norm = MinMaxScaler(feature_range=(0,1)).fit(X)
-X_minmax = pd.DataFrame(norm.transform(X), columns=X.columns)
+print(numeric_titanic.std()**2)
 
-# Standardization
+print("\n\n\n")
 
-scale = StandardScaler().fit(X)
-X_scaled = pd.DataFrame(scale.transform(X), columns=X.columns)
+#initialize it
+scaler = StandardScaler()
+#calculate all the necessary data to perform the standarization
+scaler.fit(numeric_titanic)
+#apply the standarizer to the data
+titanic_standardized = pd.DataFrame(scaler.transform(numeric_titanic),columns = numeric_cols)
+print(titanic_standardized.std()**2)
 
-print(X_minmax.describe().round(3))
-print("\n\n")
-print(X_scaled.describe().round(3))
+print("\n\n\n")
+print(titanic_standardized.head())
 
+print("\n\n\n")
+#initialize it
+scaler = MinMaxScaler()
+#calculate all the necessary data to perform the normalization
+scaler.fit(numeric_titanic)
+#apply the standarizer to the data
+titanic_normalized = pd.DataFrame(scaler.transform(numeric_titanic),columns = numeric_cols)
+print(titanic_normalized.std()**2) # seems better
 
-# Imputing Data
+print("\n\n\n")
+print(titanic_normalized.head())
 
-imputer = SimpleImputer(strategy='mean')  # Replace with mean
-X_imputed = imputer.fit_transform(X)
+print("\n\n\n\n")
+#we create a variance threshold object
+sel = VarianceThreshold(threshold=0.03)
+#we apply the method to the data - this method does not create a new dataframe, just a numpy array
+titanic_variance_removed = sel.fit_transform(titanic_normalized)
+#we can create a new dataframe using the get_feature_names_out() function to get the names of the selected columns
+titanic_dataframe_variance_removed = pd.DataFrame(titanic_variance_removed, columns = sel.get_feature_names_out())
+print(titanic_dataframe_variance_removed)
 
-# Create normalizers for L-1 and L-2
-normalizer_l1 = Normalizer(norm='l1')
-normalizer_l2 = Normalizer(norm='l2')
+print("\n\n\n\n")
 
-# Normalize data using L-1 and L-2
-normalized_data_l1 = normalizer_l1.fit_transform(X_imputed)
-normalized_data_l2 = normalizer_l2.fit_transform(X_imputed)
-
-
-
-# Observe the normalized data
-print("\n\nOriginal data:\n", X[:5])  # Print the first 5 rows
-
-print("\nL-1 normalized data:\n", normalized_data_l1[:5])
-print("\nL-1 norm sum of each column:\n", normalized_data_l1.sum(axis=0))  # Check sum of absolute values
-
-print("\n\nL-2 normalized data:\n", normalized_data_l2[:5])
-print("\nL-2 norm sum of squares of each column:\n", (normalized_data_l2**2).sum(axis=0))  # Check sum of squares
-
-
-"""
-Why we don't use Discretization'
-Limited Benefit for Most Features: In the Titanic dataset, many features (e.g., Age, Fare) likely already have a clear meaning and can be used directly in models. Discretization might not add much value.
-Potential Information Loss: Discretization can group together data points that might have different behaviors, leading to information loss.
-Choosing Cut Points: Defining meaningful cut points for discretization can be subjective and impact model performance.
-"""
-
-
-def binarize_age(age):
-    if age>=18:
-        return 1 # Adult
-    else:
-        return 0 # Child
-
-titanic['IsAdult'] = titanic['Age'].apply(binarize_age)
-
-def binarize_lonliness(row):
-    if row['SibSp']+row['Parch']==0:
-        return 1 # Alone
-    elif row['SibSp']+row['Parch']>0:
-        return 0 # Not Alone
-
-titanic['IsAlone'] = titanic.apply(binarize_lonliness, axis=1)
-
-titanic.to_csv("processed_titanic.csv", index=False)
-
-
-
-
-
-
-
-
-
-
-
+print(titanic_normalized.corr())
 
 
