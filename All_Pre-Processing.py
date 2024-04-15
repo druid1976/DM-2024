92689897fcf865724872142bda994cc796a91c3b
EmreKaratas64 <emrekaratas076@gmail.com>
2024-04-15 20:42:54 +0200

    feature transformation (embarked and sex) also saving imputed data to train data

--- All_Pre-Processing.py
+++ All_Pre-Processing.py
@@ -7,67 +7,37 @@ Created on Sat Apr  6 16:48:26 2024
 
 import pandas as pd 
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
 
 titanic = pd.read_csv("train.csv")
-categorical_features = ['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked']
-
-#print(titanic.describe())
-#print(titanic.info())
-
-
-# need to save below processings to CSV file ???
-
-# MinMax Scaling
 
 numeric_cols = titanic.select_dtypes(include=[np.number]).columns
 X = titanic[numeric_cols]
-
-norm = MinMaxScaler(feature_range=(0,1)).fit(X)
-X_minmax = pd.DataFrame(norm.transform(X), columns=X.columns)
-
-# Standardization
-
-scale = StandardScaler().fit(X)
-X_scaled = pd.DataFrame(scale.transform(X), columns=X.columns)
-
-print(X_minmax.describe().round(3))
-print("\n\n")
-print(X_scaled.describe().round(3))
-
 
 # Imputing Data
 
 imputer = SimpleImputer(strategy='mean')  # Replace with mean
 X_imputed = imputer.fit_transform(X)
+titanic[numeric_cols] = X_imputed
 
-# Create normalizers for L-1 and L-2
-normalizer_l1 = Normalizer(norm='l1')
-normalizer_l2 = Normalizer(norm='l2')
-
-# Normalize data using L-1 and L-2
-normalized_data_l1 = normalizer_l1.fit_transform(X_imputed)
-normalized_data_l2 = normalizer_l2.fit_transform(X_imputed)
-
-
-
-# Observe the normalized data
-print("\n\nOriginal data:\n", X[:5])  # Print the first 5 rows
-
-print("\nL-1 normalized data:\n", normalized_data_l1[:5])
-print("\nL-1 norm sum of each column:\n", normalized_data_l1.sum(axis=0))  # Check sum of absolute values
-
-print("\n\nL-2 normalized data:\n", normalized_data_l2[:5])
-print("\nL-2 norm sum of squares of each column:\n", (normalized_data_l2**2).sum(axis=0))  # Check sum of squares
-
-
-"""
-Why we don't use Discretization'
-Limited Benefit for Most Features: In the Titanic dataset, many features (e.g., Age, Fare) likely already have a clear meaning and can be used directly in models. Discretization might not add much value.
-Potential Information Loss: Discretization can group together data points that might have different behaviors, leading to information loss.
-Choosing Cut Points: Defining meaningful cut points for discretization can be subjective and impact model performance.
-"""
+def feature_transform(titanic_data):
+    encoder = OneHotEncoder()
+    matrix = encoder.fit_transform(titanic_data[['Embarked']]).toarray()
+    
+    column_names = ['C','S','Q','N']
+    
+    for i in range(len(matrix.T)):
+        titanic_data[column_names[i]] = matrix.T[i]
+        
+    matrix = encoder.fit_transform(titanic_data[['Sex']]).toarray()
+    column_names = ['Female','Male']
+    
+    for i in range(len(matrix.T)):
+        titanic_data[column_names[i]] = matrix.T[i]
+        
+    return titanic_data
 
 
 def binarize_age(age):
@@ -75,8 +45,7 @@ def binarize_age(age):
         return 1 # Adult
     else:
         return 0 # Child
-
-titanic['IsAdult'] = titanic['Age'].apply(binarize_age)
+    
 
 def binarize_lonliness(row):
     if row['SibSp']+row['Parch']==0:
@@ -84,9 +53,33 @@ def binarize_lonliness(row):
     elif row['SibSp']+row['Parch']>0:
         return 0 # Not Alone
 
+titanic['IsAdult'] = titanic['Age'].apply(binarize_age)
 titanic['IsAlone'] = titanic.apply(binarize_lonliness, axis=1)
 
-titanic.to_csv("processed_titanic.csv", index=False)
+titanic = feature_transform(titanic)
+
+
+#print(titanic[titanic['PassengerId']==6].Age)
+#titanic.to_csv("processed_titanic.csv", index=False)
+
+
+"""
+# MinMax Scaling
+norm = MinMaxScaler(feature_range=(0,1)).fit(X_imputed)
+X_minmax = pd.DataFrame(norm.transform(X_imputed), columns=X.columns)
+
+# Standardization
+scale = StandardScaler().fit(X_imputed)
+X_scaled = pd.DataFrame(scale.transform(X_imputed), columns=X.columns)
+
+print(X_minmax.describe().round(3))
+print("\n\n")
+print(X_scaled.describe().round(3))
+"""
+
+
+
+
 
 
 
